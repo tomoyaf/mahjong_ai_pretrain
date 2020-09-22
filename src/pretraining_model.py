fddfce39d75cb9b6ee1514ac882cdb349fef04df
tomoyaf <gazimum@gmail.com>
2020-09-23 08:13:04 +0900

    adjust hypter parameter

--- src/pretraining_model.py
+++ src/pretraining_model.py
@@ -18,8 +18,8 @@ def get_model():
     # scores(13), n_honba(3), n_round(12), sanma_or_yonma(2),
     # han_or_ton(2), aka_ari(2), kui_ari(2), special_token(4)
     config.vocab_size = 37 + 2 + 2 + 3 + 2 + 21 + 19 + 4 + 13 + 3 + 12 + 2 + 2 + 2 + 2 + 4
-    config.hidden_size = 120
-    config.num_hidden_layers = 3
+    config.hidden_size = 180
+    config.num_hidden_layers = 6
 
     discard_config = BertConfig()
     discard_config.vocab_size = config.vocab_size
