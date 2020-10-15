cddd4959e59f42d239ed982d67d3099615d081e7
tomoyaf <gazimum@gmail.com>
2020-10-16 05:39:23 +0900

    add converter

---
+++ src/convert.py
@@ -0,0 +1,30 @@
+
+import pickle
+import argparse
+import os
+from glob import glob
+from tqdm import tqdm
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_path', type=str)
+parser.add_argument('--input_path', type=str)
+parser.add_argument('--debug', type=str)
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    path_list = glob(args.input_path)
+
+    for path in tqdm(path_list):
+        with open(path, 'rb') as f:
+            paifu = pickle.load(f)
+        path_without_ext = os.path.splitext(path)[0]
+        for i, d in enumerate(paifu):
+            with open(f'{args.output_path}/{i:08}.pickle', 'wb') as f:
+                pickle.dump(d, f)
+
+
+    with open(args.debug, 'rb') as f:
+        res = pickle.load(f)
+        print(res)
