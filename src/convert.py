128d0abd68f77fd948e7de15cda2199fd7ba88bf
tomoyaf <gazimum@gmail.com>
2020-10-19 00:30:43 +0900

    convert

--- src/convert.py
+++ src/convert.py
@@ -9,11 +9,18 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument('--output_path', type=str)
 parser.add_argument('--input_path', type=str)
+parser.add_argument('--n_outputs', type=int)
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    path_list = glob(args.input_path)[:10]
+    path_list = glob(args.input_path)
+    outputs_count = 0
+
+    print(f'Input Size : {len(path_list)}')
+
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
 
     for i, path in tqdm(enumerate(path_list)):
         with open(path, 'rb') as f:
@@ -22,8 +29,9 @@ if __name__ == '__main__':
         for j, d in enumerate(paifu):
             with open(f'{args.output_path}/{i:08}_{j:08}.pickle', 'wb') as f:
                 pickle.dump(d, f)
+            outputs_count += 1
 
-
-    with open(f'{args.output_path}/00000000_00000000.pickle', 'rb') as f:
-        res = pickle.load(f)
-        print(res)
+            if outputs_count >= args.n_outputs:
+                break
+        if outputs_count >= args.n_outputs:
+            break
