951a41f0f009a148275d247801534bc7e87513ce
tomoyaf <gazimum@gmail.com>
2020-10-25 02:59:45 +0900

    add pretraining mlm

--- src/pretraining.py
+++ src/pretraining.py
@@ -2,6 +2,7 @@ import logging
 import argparse
 import catalyst
 import torch
+import math
 from torch.nn.utils.rnn import pad_sequence
 from torch.nn import functional as F
 from catalyst import dl
@@ -42,39 +43,50 @@ class CustomRunner(dl.Runner):
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--pretraining', action='store_true')
 parser.add_argument('--output_path', type=str, default='./output/pretrained')
+parser.add_argument('--pretrained_path', type=str, default='')
 parser.add_argument('--n_classes', type=int, default=37)
 parser.add_argument('--n_epochs', type=int, default=4)
 parser.add_argument('--batch_size', type=int, default=32)
-parser.add_argument('--max_data_size', type=int, default=300000)
-parser.add_argument('--lr', type=int, default=3e-5)
-parser.add_argument('--weight_decay', type=int, default=1e-2)
+parser.add_argument('--max_data_size', type=int, default=2000000)
+parser.add_argument('--lr', type=float, default=3e-5)
+parser.add_argument('--weight_decay', type=float, default=1e-2)
 parser.add_argument('--seed', type=int, default=2434)
-parser.add_argument('--model_name', type=str)
+parser.add_argument('--model_name', type=str, default='')
 args = parser.parse_args()
 
 set_seed(args.seed)
 
 print(f'device : {catalyst.utils.get_device()}')
 print(f'model : {args.model_name}')
+print(f'is_pretraining : {args.pretraining}')
 
 if __name__ == '__main__':
-    model = get_model(args.model_name)
+    model = get_model(
+        args.model_name,
+        args.pretraining,
+        args.pretrained_path
+    )
     train_loader, val_loader = get_loaders(
         args.batch_size,
         args.model_name,
-        args.max_data_size
+        args.max_data_size,
+        args.pretraining
     )
     loaders = {
         'train': train_loader,
         'valid': val_loader
     }
 
+    train_size = int(args.max_data_size * 0.9)
+    n_training_steps = math.ceil(train_size / args.batch_size)
     optimizer, lr_scheduler = get_optimizer(
         model=model,
         lr=args.lr,
         weight_decay=args.weight_decay,
-        n_epochs=args.n_epochs
+        n_epochs=args.n_epochs,
+        n_training_steps=n_training_steps
     )
 
     runner = CustomRunner(
--- src/pretraining_model.py
+++ src/pretraining_model.py
@@ -12,19 +12,26 @@ import catalyst
 from pretraining_data import PaifuDataset
 
 
-def get_model(enable_model_name):
+def get_model(enable_model_name, is_pretraining, pretrained_path):
     # tile(37), menzen(2), reach_state(2), n_reach(3),
     # reach_ippatsu(2), dans(21), rates(19), oya(4),
     # scores(13), n_honba(3), n_round(12), sanma_or_yonma(2),
     # han_or_ton(2), aka_ari(2), kui_ari(2), special_token(4)
-    vocab_size = 37 + 2 + 2 + 3 + 2 + 21 + 19 + 4 + 13 + 3 + 12 + 2 + 2 + 2 + 2 + 4
+    vocab_size = 37 + 2 + 2 + 3 + 2 + 21 + 19 + 4 + 13 + 3 + 12 + 2 + 2 + 2 + 2 + 4 # 130
     hidden_size = 1024
-    # config.hidden_size = 768
-    # config.num_attention_heads = 12
     num_attention_heads = 16
-    # config.num_hidden_layers = 2
     max_position_embeddings = 260
 
+    if is_pretraining:
+        config = BertConfig()
+        config.vocab_size = vocab_size
+        config.hidden_size = hidden_size
+        config.num_attention_heads = num_attention_heads
+        config.max_position_embeddings = max_position_embeddings
+        config.num_hidden_layers = 24
+        return MahjongPretrainingModel(config)
+
+    model = None
     if enable_model_name == 'discard':
         discard_config = BertConfig()
         discard_config.vocab_size = vocab_size
@@ -34,7 +41,7 @@ def get_model(enable_model_name):
         # discard_config.num_hidden_layers = 2
         discard_config.num_hidden_layers = 24
         # discard_config.num_hidden_layers = 12
-        return MahjongDiscardModel(discard_config)
+        model = MahjongDiscardModel(discard_config)
     elif enable_model_name == 'reach':
         reach_config = BertConfig()
         reach_config.vocab_size = vocab_size
@@ -42,7 +49,7 @@ def get_model(enable_model_name):
         reach_config.num_attention_heads = num_attention_heads
         reach_config.max_position_embeddings = max_position_embeddings
         reach_config.num_hidden_layers = 24
-        return MahjongReachChowPongKongModel(reach_config)
+        model = MahjongReachChowPongKongModel(reach_config)
     elif enable_model_name == 'chow':
         chow_config = BertConfig()
         chow_config.vocab_size = vocab_size
@@ -50,7 +57,7 @@ def get_model(enable_model_name):
         chow_config.num_attention_heads = num_attention_heads
         chow_config.max_position_embeddings = max_position_embeddings
         chow_config.num_hidden_layers = 24
-        return MahjongReachChowPongKongModel(chow_config)
+        model = MahjongReachChowPongKongModel(chow_config)
     elif enable_model_name == 'pong':
         pong_config = BertConfig()
         pong_config.vocab_size = vocab_size
@@ -58,7 +65,7 @@ def get_model(enable_model_name):
         pong_config.num_attention_heads = num_attention_heads
         pong_config.max_position_embeddings = max_position_embeddings
         pong_config.num_hidden_layers = 24
-        return MahjongReachChowPongKongModel(pong_config)
+        model = MahjongReachChowPongKongModel(pong_config)
     elif enable_model_name == 'kong':
         kong_config = BertConfig()
         kong_config.vocab_size = vocab_size
@@ -66,12 +73,21 @@ def get_model(enable_model_name):
         kong_config.num_attention_heads = num_attention_heads
         kong_config.max_position_embeddings = max_position_embeddings
         kong_config.num_hidden_layers = 24
-        return MahjongReachChowPongKongModel(kong_config)
-
-    return None
+        model = MahjongReachChowPongKongModel(kong_config)
 
 
-def get_optimizer(model, lr=1e-4, weight_decay=0.01, n_epochs=10, n_warmup_steps=1e4, n_training_steps=1e5):
+    checkpoint = torch.load(pretrained_path, map_location=catalyst.utils.get_device())
+    print(checkpoint['model_state_dict'].keys())
+    model.load_state_dict(
+        checkpoint['model_state_dict'],
+        strict=False
+    )
+
+    return model
+
+
+def get_optimizer(model, lr=1e-4, weight_decay=0.01, n_epochs=10, n_warmup_steps=1e4, n_training_steps=4e5):
+    print(f'lr:{lr}, weight_decay:{weight_decay}, n_training_steps:{n_training_steps}')
     optimizer = AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
     lr_scheduler = get_linear_schedule_with_warmup(
         optimizer,
@@ -80,21 +96,50 @@ def get_optimizer(model, lr=1e-4, weight_decay=0.01, n_epochs=10, n_warmup_steps
     )
     return optimizer, lr_scheduler
 
-
-def get_loaders(batch_size, model_name, max_data_size):
-    # path_list = glob(f'./pickle/*/paifu_2018_*.pickle')
-    path_list = glob(f'./pickle/{model_name}/*.pickle')
-    np.random.shuffle(path_list)
+def process_path_list(path_list, max_data_size):
     path_list = path_list[:max_data_size]
 
     data_size = len(path_list)
     train_size = int(data_size * 0.9)
     val_size = data_size - train_size
 
-    print(f'Data size : {data_size}, Train size : {train_size}, Val size : {val_size}')
-
     train_path_list = path_list[:train_size]
     val_path_list = path_list[-val_size:]
+
+    return train_path_list, val_path_list, data_size, train_size, val_size
+
+
+def get_loaders(batch_size, model_name, max_data_size, is_pretraining):
+    train_path_list = []
+    val_path_list = []
+    data_size = 0
+    train_size = 0
+    val_size = 0
+
+    if is_pretraining:
+        path_list_list = [
+            glob(f'./pickle/discard/*.pickle'),
+            glob(f'./pickle/reach/*.pickle'),
+            glob(f'./pickle/chow/*.pickle'),
+            glob(f'./pickle/pong/*.pickle'),
+            glob(f'./pickle/kong/*.pickle')
+        ]
+        for path_list in path_list_list:
+            train_path_list_i, val_path_list_i, data_size_i, train_size_i, val_size_i = process_path_list(path_list, max_data_size // 5)
+            train_path_list += train_path_list_i
+            val_path_list += val_path_list_i
+            data_size += data_size_i
+            train_size += train_size_i
+            val_size += val_size_i
+            print(f'Partial Data size : {data_size_i}, Partial Train Size : {train_size_i}, Partial Val Size : {val_size_i}')
+    else:
+        path_list = glob(f'./pickle/{model_name}/*.pickle')
+        train_path_list, val_path_list, data_size, train_size, val_size = process_path_list(path_list, max_data_size)
+
+    np.random.shuffle(train_path_list)
+    np.random.shuffle(val_path_list)
+
+    print(f'Data size : {data_size}, Train size : {train_size}, Val size : {val_size}')
 
     train_dataset = PaifuDataset(train_path_list)
     val_dataset = PaifuDataset(val_path_list)
@@ -413,21 +458,40 @@ class BertEncoder(nn.Module):
 
 
 class BertHead(nn.Module):
-    def __init__(self, config, output_dim):
+    def __init__(self, config):
         super().__init__()
-        self.dense0 = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dense1 = nn.Linear(config.hidden_size, output_dim)
-        self.bias = nn.Parameter(torch.zeros(output_dim))
-        self.dense1.bias = self.bias
-
-        self.act_fct = torch.tanh
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        # self.act_fct = torch.tanh
+        self.act_fct = F.gelu
         self.layer_norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
 
     def forward(self, hidden_states):
-        h = self.dense0(hidden_states[:, 0])
+        h = self.dense(hidden_states)
         h = self.act_fct(h)
         h = self.layer_norm(h)
-        h = self.dense1(h)
+        return h
+
+class MLMHead(nn.Module):
+    def __init__(self, config, output_dim):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, output_dim)
+        self.bias = nn.Parameter(torch.zeros(output_dim))
+        self.dense.bias = self.bias
+
+    def forward(self, h):
+        h = self.dense(h)
+        return h
+
+
+class ReachPongChowKongHead(nn.Module):
+    def __init__(self, config, output_dim):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, output_dim)
+        self.bias = nn.Parameter(torch.zeros(output_dim))
+        self.dense.bias = self.bias
+
+    def forward(self, h):
+        h = self.dense(h)
         return h
 
 class DiscardHead(nn.Module):
@@ -452,6 +516,94 @@ def accuracy_fct(logits, y, n_classes):
     )
 
 
+class MahjongPretrainingModel(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.output_dim = config.vocab_size
+
+        self.embeddings = MahjongEmbeddings(config)
+        self.bert_encoder = BertEncoder(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.bert_head = BertHead(config)
+        self.pretrained_model_head = MLMHead(config, config.vocab_size)
+
+        self.loss_fct = torch.nn.CrossEntropyLoss()
+        self.device = catalyst.utils.get_device()
+        self.mlm_probability = 0.15
+
+
+    def forward(self, x_features, _):
+        x, token_type_ids, pos_ids = self.embeddings.data2x(x_features, self.device)
+        x, y = self.mask_tokens(
+            x,
+            mlm_probability=self.mlm_probability,
+            special_token_id_list=self.embeddings.special_token_id_list,
+            mask_token_id=self.embeddings.mask_token_id,
+            device=self.device
+        )
+        embedding_output = self.embeddings(x, token_type_ids, pos_ids)
+
+        bert_outputs = self.bert_encoder(embedding_output)
+        sequence_output = bert_outputs[0]
+        sequence_output = self.dropout(sequence_output)
+        sequence_output = self.bert_head(sequence_output)
+        logits = self.pretrained_model_head(sequence_output)
+        loss = self.loss_fct(
+            logits.view(-1, self.config.vocab_size),
+            y.view(-1)
+        )
+        accuracy = accuracy_fct(
+            logits.view(-1, self.config.vocab_size),
+            y.view(-1),
+            self.config.vocab_size
+        )
+
+        return loss, accuracy
+
+
+    def mask_tokens(
+        self,
+        inputs,
+        mlm_probability,
+        special_token_id_list,
+        mask_token_id,
+        device
+    ):
+        labels = inputs.clone()
+
+        # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
+        probability_matrix = torch.full(labels.shape, mlm_probability, device=device)
+
+        for sp_token_id in special_token_id_list:
+            msk = labels.eq(sp_token_id)
+            probability_matrix.masked_fill_(msk, value=0.0)
+
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(
+            torch.full(labels.shape, 0.8, device=device)
+        ).bool() & masked_indices
+        inputs[indices_replaced] = mask_token_id
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(
+            torch.full(labels.shape, 0.5, device=device)
+        ).bool() & masked_indices & ~indices_replaced
+        random_words = torch.randint(
+            self.output_dim,
+            labels.shape,
+            dtype=torch.long,
+            device=device
+        )
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
+
+
 class MahjongDiscardModel(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -459,7 +611,8 @@ class MahjongDiscardModel(nn.Module):
         self.embeddings = MahjongEmbeddings(config)
         self.bert_encoder = BertEncoder(config)
         self.output_dim = 14
-        self.head = DiscardHead(config, self.output_dim)
+        self.bert_head = BertHead(config)
+        self.discard_head = DiscardHead(config, self.output_dim)
         self.loss_fct = torch.nn.CrossEntropyLoss()
 
     def forward(self, x_features, y):
@@ -468,7 +621,8 @@ class MahjongDiscardModel(nn.Module):
 
         bert_outputs = self.bert_encoder(embedding_output)
         last_hidden_state = bert_outputs[0]
-        logits = self.head(last_hidden_state)
+        last_hidden_state = self.bert_head(last_hidden_state)
+        logits = self.discard_head(last_hidden_state)
         loss = self.loss_fct(
             logits.view(-1, self.output_dim),
             y.reshape(-1)
@@ -489,7 +643,8 @@ class MahjongReachChowPongKongModel(nn.Module):
         self.embeddings = MahjongEmbeddings(config)
         self.bert_encoder = BertEncoder(config)
         self.output_dim = 2
-        self.head = BertHead(config, self.output_dim)
+        self.bert_head = BertHead(config)
+        self.reach_chow_pong_kong_head = ReachChowPongKongHead(config, self.output_dim)
         self.loss_fct = torch.nn.CrossEntropyLoss()
 
     def forward(self, x_features, y):
@@ -498,7 +653,8 @@ class MahjongReachChowPongKongModel(nn.Module):
 
         bert_outputs = self.bert_encoder(embedding_output)
         last_hidden_state = bert_outputs[0]
-        logits = self.head(last_hidden_state)
+        last_hidden_state = self.bert_head(last_hidden_state)
+        logits = self.reach_chow_pong_kong_head(last_hidden_state)
         loss = self.loss_fct(
             logits.view(-1, self.output_dim),
             y.reshape(-1)
