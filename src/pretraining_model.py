4a571b5c377a449e7fb828e6a8ab38b7acdecf6f
tomoyaf <gazimum@gmail.com>
2020-11-11 08:36:05 +0900

    L2, H16, A2

--- src/pretraining_model.py
+++ src/pretraining_model.py
@@ -21,10 +21,10 @@ def get_model(enable_model_name, is_pretraining, pretrained_path):
     vocab_size = 37 + 2 + 2 + 3 + 2 + 21 + 19 + 4 + 13 + 3 + 12 + 2 + 2 + 2 + 2 + 4 + 4 + 6 + 8 # 130 + who(4) + sum_discards(6) + shanten(8)
     # hidden_size = 1024
     # num_attention_heads = 16
-    hidden_size = 128
+    hidden_size = 16
     num_attention_heads = 2
     max_position_embeddings = 238 # base + who(1) + sum_discards(1)
-    intermediate_size = 512
+    intermediate_size = 64
     # intermediate_size = 3072
     # max_position_embeddings = 239 # base + pad(1) + who(1) + pad(1) + sum_discards(1) + pad(1) + shanten(1)
     # max_position_embeddings = 281 # 260 + pad(1) + shanten_diff(14) + pad(1) + who(1) + pad(1) + sum_discards(1) + pad(1) + shanten(1)
