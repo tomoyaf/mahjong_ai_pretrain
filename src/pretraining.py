f26e501fdda7592ced02399ba254f06a850ca7df
tomoyaf <gazimum@gmail.com>
2020-11-15 08:45:26 +0900

    acc 15

--- src/pretraining.py
+++ src/pretraining.py
@@ -18,7 +18,7 @@ import random
 class CustomRunner(dl.Runner):
 
     accumulation_steps_count = 0
-    accumulation_steps = 5
+    accumulation_steps = 15
     acc_loss = None
     acc_accuracy = None
 
@@ -109,7 +109,7 @@ if __name__ == '__main__':
         'valid': val_loader,
         'test': test_loader
     }
-    accumulation_steps = 5
+    accumulation_steps = 15
     train_size = int(args.max_data_size * 0.9)
     n_training_steps = (math.ceil(train_size // args.batch_size) // accumulation_steps) * args.n_epochs
     n_warmup_steps = int(n_training_steps * args.warmup_steps_rate)
